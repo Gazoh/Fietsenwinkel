dbb406dba20b4ea27a0b17c4c8298dad15fc4977
unknown <you@example.com>
2019-05-09 21:10:14 +0200

    Datatable toegevoegt (Nog in progress met de functies)

--- Backend/BackendGebruikers.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gazoh-PC
- * Date: 09/05/2019
- * Time: 09:24
- */
---
+++ Backend/JS/Datatable.js
@@ -0,0 +1,69 @@
+//  -------------------------------------------------------------
+// Hier begint de non Custom datatable settings
+//  -------------------------------------------------------------
+$(document).ready(function () {
+    $('#gebruikers').DataTable({
+        "language": {
+            "info": "Gebruikers _START_ tot _END_ van de _TOTAL_ in totaal"
+        },
+        // Buttons
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                text: '<span class="datatableIcon"><i class="fas fa-plus"></i></span>Toevoegen',
+                className: 'Toevoegen',
+                attr: {
+                    id: 'Toevoegen'
+                },
+                action: function (e, dt, node, config) {
+                    alert('Button activated');
+                }
+            }
+        ],
+        // Kunnen selecteren van een persoon
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        },
+        order: [[1, 'asc']
+        ]
+    });
+//  -------------------------------------------------------------
+//    Hier eindigt de non Custom datatable settings
+//  -------------------------------------------------------------
+
+//  -------------------------------------------------------------
+//    Hier begint de Custom datatable settings
+//  -------------------------------------------------------------
+
+// Append Datatable toevoegen aan een id
+    $("#Toevoegen").appendTo("#card-header");
+    $('#gebruikers_filter').appendTo('#card-header');
+
+
+    // De Options voor de 'Verwijderen' & 'Aanpassen'
+    $('<div class="selectDatatable">' +
+        '<select class="form-control">' +
+        '<option value="" disabled selected id="opties">Opties</option>' +
+        '<option value="Verwijderen" id="verwijderen">Verwijderen</option>' +
+        '<option value="Aanpassen" id="aanpassen">Aanpassen</option>' +
+        '</select>' +
+        '</div>').appendTo("#card-header");
+
+//  -------------------------------------------------------------
+//    Hier Eindigt de Custom datatable settings
+//  -------------------------------------------------------------
+
+    /*   // Data ophalen van persoon op de datatabel
+       $('#uitvoeren').on('click', function () {
+
+       });*/
+
+
+
+});
