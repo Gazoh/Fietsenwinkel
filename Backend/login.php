aa072beb06693ad1fad6204caf553487cf48f8ee
unknown <you@example.com>
2019-05-13 23:45:16 +0200

    CONFLICT MERGE TERUG NAAR 1 OUDE VERSIE

--- Backend/login.php
+++ Backend/login.php
@@ -6,47 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Hello, world!</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
     <!-- Eigen CSS-->
     <link rel="stylesheet" href="scss/backend.css">
-    <!-- Material -->
-    <link href="css/material-kit/material-kit.min.css" rel="stylesheet" />
 </head>
 <body>
-<div class="container h-100">
+<<div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
         <form class="col-12">
             <div class="form-group">
                 <div class="card border-radius07">
-                    <div class="card-header text-uppercase image-align-center card-header-image img-fluid text-white border-radius-image"
-                         style="background-image: url('assets/img/login-bg.jpg')">
+                    <div class="card-header text-uppercase image-align-center card-header-image img-fluid text-white border-radius-image" style="background-image: url('assets/img/login-bg.jpg')">
                         LOGIN
                     </div>
-                    <div class="card-body"></div>
+                    <p>test</p>
                 </div>
             </div>
+        </form>
     </div>
-    </form>
 </div>
-</div>
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
-<!--   Core JS Files   -->
-<script src="js/core/jquery.min.js" type="text/javascript"></script>
-<script src="js/core/popper.min.js" type="text/javascript"></script>
-<script src="js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-<script src="js/plugins/moment.min.js"></script>
-<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-<script src="js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="js/plugins/nouislider.min.js" type="text/javascript"></script>
-<!-- Place this tag in your head or just before your close body tag. -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-<script src="js/material-kit/material-kit.js?v=2.0.5" type="text/javascript"></script>
 </body>
 </html>
