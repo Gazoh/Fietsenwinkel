e4e06c93b8ba7dd289b58fd9292f3493c3889db4
unknown <you@example.com>
2019-05-14 19:23:25 +0200

    Login page voor CMS done

--- Backend/login.php
+++ Backend/login.php
@@ -6,29 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Hello, world!</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Eigen CSS-->
     <link rel="stylesheet" href="scss/backend.css">
+    <!-- Material Kit CSS -->
+    <link href="css/material-kit/material-kit.css?v=2.0.5" rel="stylesheet"/>
+
 </head>
 <body>
-<<div class="container h-100">
+<div class="container h-100">
     <div class="row h-100 justify-content-center align-items-center">
         <form class="col-12">
             <div class="form-group">
                 <div class="card border-radius07">
-                    <div class="card-header text-uppercase image-align-center card-header-image img-fluid text-white border-radius-image" style="background-image: url('assets/img/login-bg.jpg')">
+                    <div class="card-header text-uppercase image-align-center card-header-image img-fluid text-white border-radius-image"
+                         style="background-image: url('assets/img/login-bg.jpg')">
                         LOGIN
                     </div>
-                    <p>test</p>
+                    <form>
+                        <div class="form-group form-login">
+                            <div class="form-group bmd-form-group">
+                                <label class="bmd-label-floating">E-mail</label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group form-login">
+                            <div class="form-group bmd-form-group">
+                                <label class="bmd-label-floating">Wachtwoord</label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn main-color width300 mb-4 btn-round login-button">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </form>
     </div>
 </div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<!--   Core JS Files   -->
+<script src="JS/core/popper.min.js" type="text/javascript"></script>
+<script src="JS/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="JS/plugins/moment.min.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="JS/plugins/nouislider.min.js" type="text/javascript"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="JS/core/material-kit.js?v=2.0.5" type="text/javascript"></script>
 </body>
 </html>
