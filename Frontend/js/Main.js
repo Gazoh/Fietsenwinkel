ffb008b8e420cccbbd026e3bcd4d68762e472c6c
Gazohh <39297625+Gazohh@users.noreply.github.com>
2019-06-19 11:35:42 +0200

    Mobile filter done

--- Frontend/js/Main.js
+++ Frontend/js/Main.js
@@ -7,15 +7,3 @@ $(function () {
 $('.dropdown-menu').click(function (e) {
     e.stopPropagation();
 });
-
-// Header filter laten zien van de fietsen pagina
-$('#header-list').on('click', function () {
-    $('#mobile-header').toggleClass('showFilter');
-    $('#mobile-header-overlay').removeClass('d-none').addClass('mobile-header-overlay d-inherit');
-});
-
-// Hide Filter wanneer je op overlay klikt
-$('#mobile-header-overlay').on('click', function () {
-    $('#mobile-header-overlay').addClass('mobile-header-overlay d-none');
-    $('#mobile-header').removeClass('showFilter');
-});
